43a76dd623b31b19fc507f688296d8d0203ec1cf
lindexi <lindexi_gd@163.com>
2017-10-21 15:01:26 +0800

    Add Excerpt

---
+++ src/Mdmeta.Core/Tasks/Excerpt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Mdmeta.Tasks
+{
+    /// <summary>
+    /// 正文与摘要分割
+    /// </summary>
+    public class Excerpt
+    {
+        public Excerpt(IReadOnlyList<string> srcExcerptSeparator, string excerptSeparator)
+        {
+            SrcExcerptSeparator = srcExcerptSeparator;
+            ExcerptSeparator = excerptSeparator;
+        }
+
+        /// <summary>
+        /// 原文可能使用分割
+        /// </summary>
+        public IReadOnlyList<string> SrcExcerptSeparator { get; }
+
+        /// <summary>
+        /// 输出的分割
+        /// </summary>
+        public string ExcerptSeparator { get; }
+    }
+}
