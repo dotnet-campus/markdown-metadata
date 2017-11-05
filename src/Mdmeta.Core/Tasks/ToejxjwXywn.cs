1b0e813ca8e58256548dfbb98c8b6ae3370a7e34
lindexi <lindexi_gd@163.com>
2017-11-05 15:48:51 +0800

    获取文件时间

---
+++ src/Mdmeta.Core/Tasks/ToejxjwXywn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Mdmeta.Tasks
+{
+    /// <summary>
+    /// 获得文件的时间，修改时间
+    /// </summary>
+    public class ToejxjwXywn : MdmetaXsawJnfzmr
+    {
+        public string CreateTime { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public string Time { get; set; }
+
+        public override void Read(NghtsBdlbthhur nghtsBdlbthhur)
+        {
+            FileStream stream = nghtsBdlbthhur.Stream.BaseStream as FileStream;
+            if (stream != null)
+            {
+                var file = new FileInfo(stream.Name);
+                CreateTime = ToString(file.CreationTime);
+                Time = ToString(file.LastWriteTime);
+            }
+            ReadCsfLvi = false;
+        }
+
+        private string ToString(DateTime creationTime)
+        {
+            //2017-10-21 09:44:02 +0800
+            return
+                $"{creationTime.Year}-{creationTime.Month}-{creationTime.Day} {creationTime.Hour}:{creationTime.Minute}:{creationTime.Second} +0800";
+        }
+    }
+}
