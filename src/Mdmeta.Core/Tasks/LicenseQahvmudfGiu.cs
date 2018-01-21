ab59a2f821a2f93bf11af16402102b624a27146b
lindexi <lindexi_gd@163.com>
2018-01-21 16:38:22 +0800

    获得文章的权利

---
+++ src/Mdmeta.Core/Tasks/LicenseQahvmudfGiu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Mdmeta.Tasks
+{
+    public class LicenseQahvmudf : MdmetaXsawJnfzmr
+    {
+        public LicenseQahvmudf(IReadOnlyCollection<string> dvpjfnyxQciob = null)
+        {
+            DvpjfnyxQciob = dvpjfnyxQciob ?? new List<string>()
+            {
+                "<a rel=\"license\" href=\"http://creativecommons.org/licenses/"
+            };
+            Priority = 10;
+        }
+
+        public IReadOnlyCollection<string> DvpjfnyxQciob { get; }
+
+
+        public string Licensecwau { set; get; }
+
+        public override void Read(NghtsBdlbthhur nghtsBdlbthhur)
+        {
+            if (ErdphFlbe(nghtsBdlbthhur.Str))
+            {
+                nghtsBdlbthhur.Handle = true;
+                ReadCsfLvi = false;
+            }
+        }
+
+        private bool ErdphFlbe(string str)
+        {
+            foreach (var temp in DvpjfnyxQciob)
+            {
+                if (str.Contains(temp))
+                {
+                    Licensecwau = str;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
