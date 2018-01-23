1cc92138afdabc67d290051469257adefdb21f81
lindexi <lindexi_gd@163.com>
2018-01-23 19:24:37 +0800

    从解析的数据

---
+++ src/Mdmeta.Core/Tasks/DhhiopTbxevh.cs
@@ -0,0 +1,17 @@
+﻿namespace Mdmeta.Tasks
+{
+    internal class DhhiopTbxevh
+    {
+        public string SacdpDkqz { set; get; } =
+            @"---
+title: ""{{ Title }}""
+author: {{ Composer }}
+date: {{ date }}
+categories: {{ 标签 }}";
+
+        public void TewavuiKukm(string file)
+        {
+
+        }
+    }
+}
