4fcece21ee3d373dfccc3a518d7b0fa87ef7f054
lindexi <lindexi_gd@163.com>
2018-01-23 19:38:03 +0800

    通过标题文件

---
+++ src/Mdmeta.Core/Tasks/SzevbgsjfHpztho.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mdmeta.Tasks
+{
+    public class SzevbgsjfHpztho
+    {
+        public string HjlabDkn(HvjEthpiaca hvjEthpiaca)
+        {
+            string str = hvjEthpiaca.Title;
+            str = str.Replace(" ", "-");
+            var sxxzjDtpw = "!\t*\t\'\t(\t)\t;\t:\t@\t&\t=\t+\t$\t,\t/\t?\t#\t[\t]".Replace("\t", "");
+            //str = Uri.EscapeUriString(str);
+            foreach (var temp in sxxzjDtpw)
+            {
+                str = str.Replace(temp.ToString(), "");
+            }
+
+
+            return KwxvaSnvsyucw(hvjEthpiaca.Time) + "-" + str;
+        }
+
+        private static string KwxvaSnvsyucw(string str)
+        {
+            var dtdfKdme = DateTime.Parse(str);
+            return dtdfKdme.Year + "-" + dtdfKdme.Month + "-" + dtdfKdme.Day;
+        }
+    }
+}
