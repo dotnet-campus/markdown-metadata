e8d5025d69cd53e38f39139ddd4152e302221710
lindexi <lindexi_gd@163.com>
2018-02-13 11:30:11 +0800

    添加另外的替换

---
+++ src/Mdmeta.Core/Tasks/SszHspndy.cs
@@ -0,0 +1,22 @@
+﻿namespace Mdmeta.Tasks
+{
+    public class SszHspndy
+    {
+        public SszHspndy(HvjEthpiaca skfDqxqordlu)
+        {
+            SkfDqxqordlu = skfDqxqordlu;
+        }
+
+        public HvjEthpiaca SkfDqxqordlu {  get; }
+
+        public string DvyovKysizejh(string hfzedpSfkgf)
+        {
+            foreach (var dneStjqxmka in SkfDqxqordlu.DeopvvkHjiz)
+            {
+                hfzedpSfkgf = hfzedpSfkgf.Replace(dneStjqxmka.dkfTgnfav, dneStjqxmka.hvurSjsdt);
+            }
+
+            return hfzedpSfkgf;
+        }
+    }
+}
