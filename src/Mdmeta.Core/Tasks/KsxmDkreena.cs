7eb074decd259a53a6c87b8ec483185e164f926c
lindexi <lindexi_gd@163.com>
2018-03-14 19:45:23 +0800

    保存gitbook

---
+++ src/Mdmeta.Core/Tasks/KsxmDkreena.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Mdmeta.Tasks
+{
+    public class KsxmDkreena
+    {
+        static KsxmDkreena()
+        {
+            KrqzptgpaSoqtzniyh = Path.GetInvalidFileNameChars().ToList();
+
+            KrqzptgpaSoqtzniyh.AddRange(new[] {'+', ' ', '/', '?', '%', '#', '&', '='});
+        }
+
+        public string DmearijvSnjqishrs(HvjEthpiaca kffauwmmDgoyub)
+        {
+            var kzqDscb = kffauwmmDgoyub.Title;
+
+            if (string.IsNullOrEmpty(kzqDscb))
+            {
+                return DcmcdnhKlvcnrt();
+            }
+
+            var str = MakeValidFileName(kzqDscb, KrqzptgpaSoqtzniyh);
+
+            if (str.StartsWith("."))
+            {
+                if (str.Length > 1)
+                {
+                    str = str.Substring(1);
+                }
+                else
+                {
+                    return DcmcdnhKlvcnrt();
+                }
+            }
+
+            while (str.Contains("--"))
+            {
+                str = str.Replace("--", "-");
+            }
+
+            str = str.Trim();
+
+            if (string.IsNullOrEmpty(str))
+            {
+                return DcmcdnhKlvcnrt();
+            }
+
+            return str;
+        }
+
+        private static string DcmcdnhKlvcnrt()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        private static readonly List<char> KrqzptgpaSoqtzniyh;
+
+        private static string MakeValidFileName(string text, List<char> tizutwuysTzaduezq, string replacement = "-")
+        {
+            StringBuilder str = new StringBuilder();
+            var invalidFileNameChars = tizutwuysTzaduezq;
+            foreach (var c in text)
+            {
+                if (invalidFileNameChars.Contains(c))
+                {
+                    str.Append(replacement ?? "");
+                }
+                else
+                {
+                    str.Append(c);
+                }
+            }
+
+            return str.ToString();
+        }
+    }
+}
