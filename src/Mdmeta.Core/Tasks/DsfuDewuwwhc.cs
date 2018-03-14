902dfdc7dfbfcdf32b2af0e1dd056bdfeef151da
lindexi <lindexi_gd@163.com>
2018-03-14 19:45:08 +0800

    分开所有注释

---
+++ src/Mdmeta.Core/Tasks/DsfuDewuwwhc.cs
@@ -0,0 +1,22 @@
+﻿namespace Mdmeta.Tasks
+{
+    public class DsfuDewuwwhc : MdmetaXsawJnfzmr
+    {
+        public DsfuDewuwwhc()
+        {
+            Priority = 100;
+        }
+
+        public override void Read(NghtsBdlbthhur nghtsBdlbthhur)
+        {
+            var dkhxiHypu = nghtsBdlbthhur.Str.Trim();
+            const string higmvucSgbavup = "<!--";
+            const string kewxbHwwdpafd = "-->";
+            if (dkhxiHypu.StartsWith(higmvucSgbavup) && dkhxiHypu.EndsWith(kewxbHwwdpafd))
+            {
+                var dzxvbfgKjjctb = dkhxiHypu.Replace(higmvucSgbavup, "").Replace(kewxbHwwdpafd, "").Trim();
+                HvjEthpiaca.HqshpnjiKlclzh.Add(dzxvbfgKjjctb);
+            }
+        }
+    }
+}
