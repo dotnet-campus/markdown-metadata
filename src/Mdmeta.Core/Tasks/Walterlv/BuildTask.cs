9e45e1062b8ff61705d681333b874fa9c21e8095
walterlv <xblero@gmail.com>
2018-08-12 16:11:30 +0800

    Add front matter reader.

--- src/Mdmeta.Core/Tasks/Walterlv/BuildTask.cs
+++ src/Mdmeta.Core/Tasks/Walterlv/BuildTask.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using Mdmeta.Core;
+using YamlDotNet.Serialization;
 
 namespace Mdmeta.Tasks.Walterlv
 {
@@ -23,19 +24,25 @@ namespace Mdmeta.Tasks.Walterlv
             return 0;
         }
 
-        private static Dictionary<string, string> ReadFrontMatter(FileInfo file)
+        private static YamlFrontMeta ReadFrontMatter(FileInfo file)
         {
-            bool? containsYamlMatter = null;
-            var frontMatter = new Dictionary<string, string>();
+            var yaml = FindYamlFrontMatter(file);
+
+            var deserializer = new Deserializer();
+            var matter = deserializer.Deserialize<YamlFrontMeta>(yaml);
+
+            return matter;
+        }
+
+        private static string FindYamlFrontMatter(FileInfo file)
+        {
+            var yamlLines = new List<string>();
 
             using (var fileStream = file.OpenRead())
             using (var reader = new StreamReader(fileStream, Encoding.UTF8, true))
             {
-                var lineIndex = 0;
-                var keyDepth = 0;
+                bool? containsYamlMatter = null;
                 var line = reader.ReadLine()?.Trim();
-
-
                 while (line != null)
                 {
                     if (containsYamlMatter == null)
@@ -43,6 +50,7 @@ namespace Mdmeta.Tasks.Walterlv
                         if (line == "---")
                         {
                             containsYamlMatter = true;
+                            line = reader.ReadLine()?.Trim();
                             continue;
                         }
 
@@ -52,34 +60,96 @@ namespace Mdmeta.Tasks.Walterlv
 
                     if (containsYamlMatter is true)
                     {
-                        var splitIndex = line.IndexOf(':');
-                        if (splitIndex > 0)
+                        if (line == "---")
                         {
-                            var key = line.Substring(0, splitIndex).Trim();
-                            if (splitIndex + 1 < line.Length)
-                            {
-                                // key: value
-                                var value = line.Substring(splitIndex + 1).Trim();
-                                frontMatter[key] = value;
-                            }
-                            else
-                            {
-                                // key:
-                                //   value
-                                keyDepth++;
-                            }
-                        }
-                        else
-                        {
-                            
+                            break;
                         }
 
-                        throw new NotSupportedException($"在 YAML 元数据中没有找到 key。文件 {file.FullName}，行 {lineIndex + 1}。");
+                        yamlLines.Add(line);
                     }
+
+                    line = reader.ReadLine()?.Trim();
                 }
             }
 
-            return frontMatter;
+            var builder = new StringBuilder();
+            foreach (var line in yamlLines)
+            {
+                builder.AppendLine(line);
+            }
+            return builder.ToString();
         }
+    }
+
+    public sealed class YamlFrontMeta
+    {
+        [YamlMember(Alias = "title", ApplyNamingConventions = false)]
+        public string Title { get; set; }
+
+        [YamlMember(Alias = "date", ApplyNamingConventions = false)]
+        public string Date { get; set; }
+
+        [YamlMember(Alias = "date_published", ApplyNamingConventions = false)]
+        public string PublishDate { get; set; }
+
+        [YamlMember(Alias = "layout", ApplyNamingConventions = false)]
+        public string Layout { get; set; }
+
+        [YamlMember(Alias = "permalink", ApplyNamingConventions = false)]
+        public string PermanentUrl { get; set; }
+
+        [YamlMember(Alias = "categories", ApplyNamingConventions = false)]
+        public string Categories { get; set; }
+
+        [YamlMember(Alias = "tags", ApplyNamingConventions = false)]
+        public string Tags { get; set; }
+
+        [YamlMember(Alias = "keywords", ApplyNamingConventions = false)]
+        public string Keywords { get; set; }
+
+        [YamlMember(Alias = "description", ApplyNamingConventions = false)]
+        public string Description { get; set; }
+
+        [YamlMember(Alias = "version", ApplyNamingConventions = false)]
+        public List<VersionInfo> Version { get; set; }
+
+        [YamlMember(Alias = "versions", ApplyNamingConventions = false)]
+        public List<VersionsInfo> Versions { get; set; }
+
+        [YamlMember(Alias = "published", ApplyNamingConventions = false)]
+        public bool IsPublished { get; set; } = true;
+
+        [YamlMember(Alias = "sitemap", ApplyNamingConventions = false)]
+        public bool IsInSiteMap { get; set; } = true;
+    }
+
+    public sealed class VersionInfo
+    {
+        [YamlMember(Alias = "current", ApplyNamingConventions = false)]
+        public string Current { get; set; }
+    }
+
+    public sealed class VersionsInfo
+    {
+        [YamlMember(Alias = "中文", ApplyNamingConventions = false)]
+        public string Chinese { get; set; }
+
+        [YamlMember(Alias = "English", ApplyNamingConventions = false)]
+        public string English { get; set; }
+
+        [YamlMember(Alias = "русский", ApplyNamingConventions = false)]
+        public string Sample0 { get; set; }
+
+        [YamlMember(Alias = "繁體中文", ApplyNamingConventions = false)]
+        public string Sample1 { get; set; }
+
+        [YamlMember(Alias = "简体中文", ApplyNamingConventions = false)]
+        public string Sample2 { get; set; }
+
+        [YamlMember(Alias = "日本語", ApplyNamingConventions = false)]
+        public string Sample3 { get; set; }
+
+        [YamlMember(Alias = "ไทย", ApplyNamingConventions = false)]
+        public string Sample4 { get; set; }
     }
 }
