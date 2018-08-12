8b341c935df485a8ff37416767d8b8fff141d96a
walterlv <xblero@gmail.com>
2018-08-12 16:31:28 +0800

    Finish file date modifying.

--- src/Mdmeta.Core/Tasks/Walterlv/BuildTask.cs
+++ src/Mdmeta.Core/Tasks/Walterlv/BuildTask.cs
@@ -1,81 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using Mdmeta.Core;
-using YamlDotNet.Serialization;
 
 namespace Mdmeta.Tasks.Walterlv
 {
-    [CommandMetadata("winit", Description = "将所有的 md 文件按照 YAML 元数据设置文件属性。")]
-    public sealed class InitTask : CommandTask
+    [CommandMetadata("wbuild",
+        Description = "Add metadata to markdown files.")]
+    public sealed class BuildTask : CommandTask
     {
-        [CommandArgument("folder", Description = "要初始化 md 文件属性的文件夹。")]
-        public string FolderName { get; set; }
+        [CommandArgument("file",
+            Description = "Which file to add markdown.")]
+        public string FileOrFolderName { get; set; }
+
+        [CommandOption("--override",
+            Description = "Indicate whether to override files that already has front matter or not.")]
+        public bool Override { get; set; }
 
         public override int Run()
         {
-            var folder = new DirectoryInfo(Path.GetFullPath(FolderName));
-            foreach (var file in folder.EnumerateFiles("*.md", SearchOption.AllDirectories))
-            {
-                var frontMatter = ReadFrontMatter(file);
-            }
-
+            Console.WriteLine($"{FileOrFolderName}");
             return 0;
-        }
-
-        private static YamlFrontMeta ReadFrontMatter(FileInfo file)
-        {
-            var yaml = FindYamlFrontMatter(file);
-
-            var deserializer = new Deserializer();
-            var matter = deserializer.Deserialize<YamlFrontMeta>(yaml);
-
-            return matter;
-        }
-
-        private static string FindYamlFrontMatter(FileInfo file)
-        {
-            var yamlLines = new List<string>();
-
-            using (var fileStream = file.OpenRead())
-            using (var reader = new StreamReader(fileStream, Encoding.UTF8, true))
-            {
-                bool? containsYamlMatter = null;
-                var line = reader.ReadLine()?.Trim();
-                while (line != null)
-                {
-                    if (containsYamlMatter == null)
-                    {
-                        if (line == "---")
-                        {
-                            containsYamlMatter = true;
-                            line = reader.ReadLine()?.Trim();
-                            continue;
-                        }
-
-                        containsYamlMatter = false;
-                        break;
-                    }
-
-                    if (line != "---")
-                    {
-                        yamlLines.Add(line);
-                        line = reader.ReadLine()?.Trim();
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-            }
-
-            var builder = new StringBuilder();
-            foreach (var line in yamlLines)
-            {
-                builder.AppendLine(line);
-            }
-            return builder.ToString();
         }
     }
 }
--- src/Mdmeta.Core/Tasks/Walterlv/InitTask.cs
+++ src/Mdmeta.Core/Tasks/Walterlv/InitTask.cs
@@ -1,24 +1,96 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using Mdmeta.Core;
+using YamlDotNet.Serialization;
 
 namespace Mdmeta.Tasks.Walterlv
 {
-    [CommandMetadata("wbuild",
-        Description = "Add metadata to markdown files.")]
-    public sealed class BuildTask : CommandTask
+    [CommandMetadata("winit", Description = "将所有的 md 文件按照 YAML 元数据设置文件属性。")]
+    public sealed class InitTask : CommandTask
     {
-        [CommandArgument("file",
-            Description = "Which file to add markdown.")]
-        public string FileOrFolderName { get; set; }
-
-        [CommandOption("--override",
-            Description = "Indicate whether to override files that already has front matter or not.")]
-        public bool Override { get; set; }
+        [CommandArgument("folder", Description = "要初始化 md 文件属性的文件夹。")]
+        public string FolderName { get; set; }
 
         public override int Run()
         {
-            Console.WriteLine($"{FileOrFolderName}");
+            var folder = new DirectoryInfo(Path.GetFullPath(FolderName));
+            foreach (var file in folder.EnumerateFiles("*.md", SearchOption.AllDirectories))
+            {
+                var frontMatter = ReadFrontMatter(file);
+
+                var dateString = frontMatter.Date;
+                var publishDateString = frontMatter.PublishDate ?? dateString;
+
+                var date = DateTimeOffset.Parse(dateString);
+                var publishDate = DateTimeOffset.Parse(publishDateString);
+
+                FixFileDate(file, publishDate, date);
+            }
+
             return 0;
+        }
+
+        private void FixFileDate(FileInfo file, DateTimeOffset createdTime, DateTimeOffset modifiedTime)
+        {
+            file.CreationTimeUtc = createdTime.UtcDateTime;
+            file.LastWriteTimeUtc = modifiedTime.UtcDateTime;
+            file.LastAccessTimeUtc = DateTimeOffset.Now.UtcDateTime;
+        }
+
+        private static YamlFrontMeta ReadFrontMatter(FileInfo file)
+        {
+            var yaml = FindYamlFrontMatter(file);
+
+            var deserializer = new Deserializer();
+            var matter = deserializer.Deserialize<YamlFrontMeta>(yaml);
+
+            return matter;
+        }
+
+        private static string FindYamlFrontMatter(FileInfo file)
+        {
+            var yamlLines = new List<string>();
+
+            using (var fileStream = file.OpenRead())
+            using (var reader = new StreamReader(fileStream, Encoding.UTF8, true))
+            {
+                bool? containsYamlMatter = null;
+                var line = reader.ReadLine()?.Trim();
+                while (line != null)
+                {
+                    if (containsYamlMatter == null)
+                    {
+                        if (line == "---")
+                        {
+                            containsYamlMatter = true;
+                            line = reader.ReadLine()?.Trim();
+                            continue;
+                        }
+
+                        containsYamlMatter = false;
+                        break;
+                    }
+
+                    if (line != "---")
+                    {
+                        yamlLines.Add(line);
+                        line = reader.ReadLine()?.Trim();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var builder = new StringBuilder();
+            foreach (var line in yamlLines)
+            {
+                builder.AppendLine(line);
+            }
+            return builder.ToString();
         }
     }
 }
