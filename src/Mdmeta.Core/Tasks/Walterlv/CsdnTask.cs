ef18e4e9a1edf81fa00db76c807dc1bb2d1aba08
walterlv <xblero@gmail.com>
2018-10-28 14:38:01 +0800

    Add upload util.

---
+++ src/Mdmeta.Core/Tasks/Walterlv/CsdnTask.cs
@@ -0,0 +1,48 @@
+﻿using Mdmeta.Core;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using static Mdmeta.Tasks.Walterlv.MdmetaUtils;
+
+namespace Mdmeta.Tasks.Walterlv
+{
+    [CommandMetadata("wcsdn", Description = "将文件转换为 CSDN 博客能原生支持的 Markdown 格式。")]
+    public class CsdnTask : CommandTask
+    {
+        [CommandArgument("[file]", Description = "要转换格式的文件的完全限定路径。")]
+        public string FileName { get; set; }
+
+        [CommandOption("-b|--image-base-path", Description = "图片在本地文件系统中的基地址。")]
+        public string ImageBasePath { get; set; }
+
+        public override int Run()
+        {
+            if (!File.Exists(FileName))
+            {
+                OutputError("文件不存在。");
+                return 4;
+            }
+
+            var file = new FileInfo(FileName);
+
+            var text = File.ReadAllText(file.FullName, Encoding.UTF8);
+            var imageRegex = new Regex(@"!\[(?<name>.+)\]\((?<path>/static/posts/[\d-]+\.png)\)");
+            var matches = imageRegex.Matches(text);
+            foreach (Match match in matches)
+            {
+                var name = match.Groups["name"].Value;
+                var path = match.Groups["path"].Value;
+
+                var smms = new Smms();
+                var localImagePath = Path.GetFullPath(ImageBasePath+ path);
+                var uploadedUrl = smms.UploadAsync(localImagePath).Result;
+
+                text = text.Replace(text, uploadedUrl);
+            }
+
+            File.WriteAllText(file.FullName, text, Encoding.UTF8);
+
+            return 0;
+        }
+    }
+}
---
+++ src/Mdmeta.Core/Tasks/Walterlv/Smms.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Mdmeta.Tasks.Walterlv
+{
+    public sealed class Smms
+    {
+        public async Task<string> UploadAsync(string localImagePath)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://sm.ms");
+            client.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36");
+
+            string result;
+            using (var stream = File.OpenRead(localImagePath))
+            {
+                HttpContent content = new StreamContent(stream);
+                var form = new MultipartFormDataContent();
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                form.Add(content, "smfile", "123");
+                var response = await client.PostAsync("/api/upload", form);
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+
+            return result;
+        }
+    }
+}
