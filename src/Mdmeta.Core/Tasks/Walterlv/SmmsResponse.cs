ad12f189dce7d8e1b799b13e6a83269767fdcc3c
walterlv <xblero@gmail.com>
2018-10-28 14:49:10 +0800

    Add json model class.

---
+++ src/Mdmeta.Core/Tasks/Walterlv/SmmsResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Mdmeta.Tasks.Walterlv
+{
+    public class Data
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string filename { get; set; }
+        public string storename { get; set; }
+        public int size { get; set; }
+        public string path { get; set; }
+        public string hash { get; set; }
+        public int timestamp { get; set; }
+        public string ip { get; set; }
+        public string url { get; set; }
+        public string delete { get; set; }
+    }
+
+    public class SmmsResponse
+    {
+        public string code { get; set; }
+        public Data data { get; set; }
+    }
+}
