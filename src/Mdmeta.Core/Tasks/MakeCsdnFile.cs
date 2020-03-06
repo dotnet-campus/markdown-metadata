23dac72f60a1591796bd13c1dfebe363090b8707
lindexi <lindexi_gd@163.com>
2020-03-06 08:47:45 +0800

    创建用来上传csdn文件

--- src/Mdmeta.Core/Tasks/MakeCsdnFile.cs
+++ src/Mdmeta.Core/Tasks/MakeCsdnFile.cs
@@ -1,22 +1,11 @@
 ﻿namespace Mdmeta.Tasks
 {
-    public interface IMakeFileName
-    {
-        /// <summary>
-        /// 获取文件名不加后缀
-        /// </summary>
-        /// <param name="hvjEthpiaca"></param>
-        /// <returns></returns>
-        string MakeFileName(HvjEthpiaca hvjEthpiaca);
-    }
-
     public class MakeCsdnFile : IMakeFileName
     {
         /// <inheritdoc />
         public string MakeFileName(HvjEthpiaca hvjEthpiaca)
         {
             string str = hvjEthpiaca.Title;
-            str = str.Replace(" ", "-");
 
             str = ValidFileName.MakeValidFileName(str);
             return str;
