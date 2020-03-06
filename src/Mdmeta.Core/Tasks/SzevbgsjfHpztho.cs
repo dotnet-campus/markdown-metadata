8ff816429aa2594c3f85783f4951b929eaf25b2a
lindexi <lindexi_gd@163.com>
2020-03-06 08:44:48 +0800

    格式代码

--- src/Mdmeta.Core/Tasks/SzevbgsjfHpztho.cs
+++ src/Mdmeta.Core/Tasks/SzevbgsjfHpztho.cs
@@ -1,33 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Mdmeta.Tasks
 {
-    public static class ValidFileName
-    {
-        public static string MakeValidFileName(string text, string replacement = "_")
-        {
-            StringBuilder str = new StringBuilder();
-            var invalidFileNameChars = System.IO.Path.GetInvalidFileNameChars();
-            foreach (var c in text)
-            {
-                if (invalidFileNameChars.Contains(c))
-                {
-                    str.Append(replacement ?? "");
-                }
-                else
-                {
-                    str.Append(c);
-                }
-            }
-
-            return str.ToString();
-        }
-    }
-
-    public class SzevbgsjfHpztho: IMakeFileName
+    public class SzevbgsjfHpztho : IMakeFileName
     {
         public string MakeFileName(HvjEthpiaca hvjEthpiaca)
         {
