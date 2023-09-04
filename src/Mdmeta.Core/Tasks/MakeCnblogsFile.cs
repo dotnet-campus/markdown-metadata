0b660201e55740d935d379ca09cc18581af32c86
lindexi <lindexi_gd@163.com>
2023-09-04 17:42:25 +0800

    支持配置标题

---
+++ src/Mdmeta.Core/Tasks/MakeCnblogsFile.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace Mdmeta.Tasks
+{
+    public class MakeCnblogsFile : IMakeFileName
+    {
+        public string MakeFileName(HvjEthpiaca hvjEthpiaca)
+        {
+            string str = hvjEthpiaca.DeopvvkHjiz.FirstOrDefault(t => t.dkfTgnfav == "标题").hvurSjsdt;
+
+            if (string.IsNullOrEmpty(str))
+            {
+                str = hvjEthpiaca.Title;
+            }
+
+            str = ValidFileName.MakeValidFileName(str);
+            return str;
+        }
+    }
+}
